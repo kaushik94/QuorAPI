41d238a27bb40a645045be225fa5430525332d2f
kaushik94 <kaushik.varanasi1@gmail.com>
2015-04-27 07:34:00 +0530

    doctest examples

--- HTMLparser.py
+++ HTMLparser.py
@@ -13,7 +13,7 @@ def get_answer(url):
 	>>> from QuorAPI.HTMLParser import get_answer
 	>>> url = 'http://www.quora.com/Google-India-Intern-2015-Who-all-will-be-joining-as-interns-at-Google-India-Office-s-in-summer-2015/answer/Kaushik-Varanasi'
 	>>> get_answer(url)
-<html><b><span>6</span> Upvotes</b><p><div class="ExpandedQText QuotableExpandedAnswer ExpandedAnswer"><span class="qlink_container"><a href="/Dhruvi-Shah-2">Dhruvi Shah</a></span> - DA-IICT<div class="ContentFooter AnswerFooter"><span id="ld_kbglrj_100065"><a action_mousedown="AnswerPermalinkClickthrough" class="answer_permalink" href="/Google-India-Intern-2015-Who-all-will-be-joining-as-interns-at-Google-India-Office-s-in-summer-2015/answer/Kaushik-Varanasi" 	id="__w2_SgsCtB8_link">Written Fri</a></span>. 127 views<span id="ld_kbglrj_100066"></span>.</div></div></p></html>
+	<html><b><span>6</span> Upvotes</b><p><div class="ExpandedQText QuotableExpandedAnswer ExpandedAnswer"><span class="qlink_container"><a href="/Dhruvi-Shah-2">Dhruvi Shah</a></span> - DA-IICT<div class="ContentFooter AnswerFooter"><span id="ld_kbglrj_100065"><a action_mousedown="AnswerPermalinkClickthrough" class="answer_permalink" href="/Google-India-Intern-2015-Who-all-will-be-joining-as-interns-at-Google-India-Office-s-in-summer-2015/answer/Kaushik-Varanasi" 	id="__w2_SgsCtB8_link">Written Fri</a></span>. 127 views<span id="ld_kbglrj_100066"></span>.</div></div></p></html>
 
 	'''
 
@@ -35,7 +35,7 @@ def get_profile(url):
 	>>> from QuorAPI.HTMLParser import get_profile
 	>>> url = 'http://www.quora.com/Gayle-Laakmann-McDowell'
 	>>> get_profile(url)
-<html><center><p><h1><div class="hover_menu hidden show_nub" id="__w2_Fw9HCKm_menu"><div class="hover_menu_contents white_bg" id="__w2_Fw9HCKm_menu_contents"> </div></div><span class="user">Gayle Laakmann McDowell</span><span id="ld_yyouqm_2351"></span><span class="UserTopWriterBadge"><span class="CurrentTopWriterIcon TopWriterIcon" id="__w2_OeKl0BZ_icon"></span></span></h1></p><p><img alt="Gayle Laakmann McDowell" class="profile_photo_img" height="200" src="http://qph.is.quoracdn.net/main-thumb-47758-200-trqmqasqcepzfhlzoohtrofqpkqetigv.jpeg" width="200"/></p><b><p>45.6k Followers</p></b></center></html>
+	<html><center><p><h1><div class="hover_menu hidden show_nub" id="__w2_Fw9HCKm_menu"><div class="hover_menu_contents white_bg" id="__w2_Fw9HCKm_menu_contents"> </div></div><span class="user">Gayle Laakmann McDowell</span><span id="ld_yyouqm_2351"></span><span class="UserTopWriterBadge"><span class="CurrentTopWriterIcon TopWriterIcon" id="__w2_OeKl0BZ_icon"></span></span></h1></p><p><img alt="Gayle Laakmann McDowell" class="profile_photo_img" height="200" src="http://qph.is.quoracdn.net/main-thumb-47758-200-trqmqasqcepzfhlzoohtrofqpkqetigv.jpeg" width="200"/></p><b><p>45.6k Followers</p></b></center></html>
 
 	'''
 
